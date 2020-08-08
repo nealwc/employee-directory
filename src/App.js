94e6c53719ebabe85e0df37495a565b70312fb73
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-08-07 21:24:31 -0400

    supervisor filters from data not state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import React, { Component } from "react";
 import Jumbotron from "./components/Jumbotron";
 import EmployeeCard from "./components/EmployeeCard";
-import employees from "./employees.json";
+import employeesData from "./employeesData.json";
 import Wrapper from "./components/Wrapper";
 import ButtonGroup from "./components/ButtonGroup";
 
 class App extends Component {
     state = {
-        employees
+        employees : employeesData
     };
+
+    // componentDidMount() {
+    //     this.loadEmployees();
+    //   }
 
     filterBySupervisor = e => {
         const supervisor = e.target.value
 
-        const employees = this.state.employees.filter(employee => employee.supervisor === supervisor);
+        const employees = employeesData.filter(employee => employee.supervisor === supervisor);
         this.setState({ employees })
     };
 
     sortByFirstName = e => {
-        const employeesByFirstName = employees.sort((a, b) => (a["firstName"].charAt(0) > b["firstName"].charAt(0)) ? 1 : -1);
+        const employeesByFirstName = this.state.employees.sort((a, b) => (a["firstName"].charAt(0) > b["firstName"].charAt(0)) ? 1 : -1);
             this.setState({ employeesByFirstName })
     };
 
     sortByLastName = e => {
-        const employeesByLastName = employees.sort((a, b) => (a["lastName"].charAt(0) > b["lastName"].charAt(0)) ? 1 : -1);
+        const employeesByLastName = this.state.employees.sort((a, b) => (a["lastName"].charAt(0) > b["lastName"].charAt(0)) ? 1 : -1);
             this.setState({ employeesByLastName })
     };
+
+    // loadNextDog = () => {
+    //     API.getRandomDog()
+    //       .then(res =>
+    //         this.setState({
+    //           image: res.data.message
+    //         })
+    //       )
+    //       .catch(err => console.log(err));
+    //   };
 
     render() {
         return (
